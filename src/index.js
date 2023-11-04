a82048e3ff0ee5ce12fa8457d45e2183d62a9e4d
Alex <alexdescobars@gmail.com>
2023-11-04 12:12:21 +0000

    Adding a lot changes to the worker with r2

--- src/index.js
+++ src/index.js
@@ -8,8 +8,58 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+
+// Check requests for a pre-shared secret
+const hasValidHeader = (request, env) => {
+	return request.headers.get('X-Custom-Auth-Key') === env.AUTH_KEY_SECRET;
+  };
+
+  function authorizeRequest(request, env, key) {
+	switch (request.method) {
+	  case 'GET':
+		return hasValidHeader(request, env);
+	  default:
+		return false;
+	}
+  }
+
+
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+	async fetch(request, env) {
+	  const url = new URL(request.url);
+	  const key = url.pathname.slice(1);
+  
+	// If the "X-Custom-Auth-Key" Header is not present or valid, a "Forbidden" will be observed.
+	  if (!authorizeRequest(request, env, key)) {
+		return new Response('Forbidden', { status: 403 });
+	  }
+
+	// If the "X-Custom-Auth-Key" Header was present, then we evaluate the HTTP method.
+	// Kept only the GET for simplicity and as per requirement, but used a case for future use cases.
+	  switch (request.method) {
+	
+		case 'GET':
+		  const object = await env.MY_COUNTRY_FLAGS_BUCKET.get(key);
+  
+		  if (object === null) {
+			return new Response('Object Not Found', { status: 404 });
+		  }
+  
+		  const headers = new Headers();
+		  object.writeHttpMetadata(headers);
+		  headers.set('etag', object.httpEtag);
+  
+		  return new Response(object.body, {
+			headers,
+		  });
+  
+		default:
+		  return new Response('Method Not Allowed', {
+			status: 405,
+			headers: {
+			  Allow: 'PUT, GET, DELETE',  
+			},
+		  });
+	  }
 	},
-};
+  };
